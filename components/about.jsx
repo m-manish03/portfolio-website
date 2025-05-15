e66bbfb10fc93c5160365de95c3a9c002076eb72
Manish <87753285+m-manish03@users.noreply.github.com>
2025-05-15 16:55:55 +0530

    Update about.jsx

--- components/about.jsx
+++ components/about.jsx
@@ -62,7 +62,7 @@ export default function About() {
               className="relative h-80 md:h-96 rounded-lg overflow-hidden border-2 border-gray-200 dark:border-gray-800 opacity-0 animate-scale stagger-1"
             >
               <Image
-                src="/profile.jpg?height=400&width=300"
+                src="/profile.jpg?height=300&width=400"
                 alt="Developer portrait"
                 fill
                 className="object-cover"
